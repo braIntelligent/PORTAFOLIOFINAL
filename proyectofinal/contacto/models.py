36fd86c07a06502cd7fc6b0d54af81207dd4b4c3
braIntelligent <m.cataldoolate@gmail.com>
2024-05-27 11:45:33 -0400

    editdb

--- proyectofinal/contacto/models.py
+++ proyectofinal/contacto/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Contacto(models.Model):
     nombre = models.CharField(max_length=50)
-    correo = models.EmailField(max_length=50, unique=True)
+    correo = models.EmailField(max_length=50)
     mensaje = models.TextField()
     date = models.DateTimeField(auto_now=True)
 
